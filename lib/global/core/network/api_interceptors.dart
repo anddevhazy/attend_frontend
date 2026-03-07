ea4d7161c6d9cb93c67757763f5f67ab870a037d
Sodipe Oluwasina Ayowole <haywhy2946@gmail.com>
2026-03-07 13:19:10 +0100

    OUJFR feat: dependency injection - DONE

--- lib/global/core/network/api_interceptors.dart
+++
@@ -1,5 +0,0 @@
-import 'package:dio/dio.dart';
-
-class AuthInterceptor extends Interceptor {
-  AuthInterceptor(param0);
-}
--- lib/main_injection_container.dart
+++ lib/main_injection_container.dart
@@ -1,11 +1,11 @@
-import 'package:attend/features/lecturer/lecturer_injection_container.dart';
-import 'package:attend/global/core/interceptors/refresh_token_interceptor.dart';
-import 'package:attend/global/core/network/api_interceptors.dart';
-import 'package:attend/features/auth/auth_injection_container.dart';
 import 'package:attend/features/auth/data/data_sources/local/auth_local_data_source.dart';
-import 'package:attend/service_locator.dart';
+import 'package:attend/features/lecturer/lecturer_injection_container.dart';
+import 'package:attend/global/core/interceptors/auth_interceptor.dart';
+import 'package:attend/global/core/interceptors/refresh_token_interceptor.dart';
+import 'package:attend/features/auth/auth_injection_container.dart';
 import 'package:attend/global/storage/token_storage.dart';
 import 'package:attend/global/storage/token_storage_impl.dart';
+import 'package:attend/service_locator.dart';
 import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
@@ -36,21 +36,22 @@ Future<void> init() async {
   //google sign in
   final googleSignIn = GoogleSignIn.instance;
   sl.registerLazySingleton(() => googleSignIn);
-
-  sl.registerLazySingleton<FlutterSecureStorage>(() => FlutterSecureStorage());
-
+  // token storage
   sl.registerLazySingleton<TokenStorage>(
     () => TokenStorageImpl(sl<FlutterSecureStorage>()),
   );
+  // flutter secure storage
+  sl.registerLazySingleton<FlutterSecureStorage>(() => FlutterSecureStorage());
 
   sl.registerLazySingleton<AuthInterceptor>(
-    () => AuthInterceptor(sl<AuthLocalDataSource>()),
+    () => AuthInterceptor(localDataSource: sl<AuthLocalDataSource>()),
   );
-
   sl.registerLazySingleton<RefreshTokenInterceptor>(
     () => RefreshTokenInterceptor(sl<Dio>(), sl<AuthLocalDataSource>()),
   );
-
   await authInjectionContainer();
   await lecturerInjectionContainer();
 }
+
+
+// note for me: you create an instance when it's something coming from the outside, like google sign in, dio, connectivity, flutter secure storage. You don't create an instance for your own classes, you just register them.
